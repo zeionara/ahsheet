fd47175d2b39f409564bf98059ec47e0749f8ae9
Dmitrii <zeionara@gmail.com>
2021-08-06 22:34:48 +0300

    Added public constructor for SheetData structure

--- Sources/ahsheet/models/values/SheetData.swift
+++ Sources/ahsheet/models/values/SheetData.swift
@@ -1,4 +1,9 @@
 public struct SheetData: Codable {
     let range: String
     let values: [[String]] // The first dimension corresponds to rows, the second - to columns 
+
+    public init(range: String, values: [[String]]) {
+        self.range = range
+        self.values = values
+    }
 }
