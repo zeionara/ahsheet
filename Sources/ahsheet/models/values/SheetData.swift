10678ef27473f5060209aa8134fb309145a825c5
Dmitrii <zeionara@gmail.com>
2021-08-07 21:52:59 +0300

    Changed accessibility of SheetData properties

--- Sources/ahsheet/models/values/SheetData.swift
+++ Sources/ahsheet/models/values/SheetData.swift
@@ -1,6 +1,6 @@
 public struct SheetData: Codable {
-    let range: String
-    let values: [[String]] // The first dimension corresponds to rows, the second - to columns 
+    public let range: String
+    public let values: [[String]] // The first dimension corresponds to rows, the second - to columns 
 
     public init(range: String, values: [[String]]) {
         self.range = range
